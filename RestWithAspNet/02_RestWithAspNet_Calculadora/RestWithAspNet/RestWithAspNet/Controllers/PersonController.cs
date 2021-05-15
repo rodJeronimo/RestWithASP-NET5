92f813e47f92e13b2c56dd9d24e7b584f40bd0bc
Rodrigo Jerônimo <rodrigo.jeronimo@msn.com>
2021-05-15 16:48:04 -0300

    Working with 4 verbs

---
+++ RestWithAspNet/02_RestWithAspNet_Calculadora/RestWithAspNet/RestWithAspNet/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum()
+        {   
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ RestWithAspNet/02_RestWithAspNet_Calculadora/RestWithAspNet/RestWithAspNet/Model/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet.Model
+{
+    public class Person
+    {
+
+        public long ID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
